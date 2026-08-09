da33a825b5644315cdf98a3ae7ac813b4a92e8ed
sandip2007-ncu <sandip25csu215@ncuindia.edu>
2026-08-09 14:30:51 +0530

    Refactor LeapYear to include printLeapYear method
    Refactor LeapYear class to use a method for checking leap year.

--- Week_2/LeapYear.java
+++ Week_2/LeapYear.java
@@ -1,10 +1,18 @@
 import java.util.Scanner;
-public class LeapYear{
-  public static void main(String[] args){
-    Scanner input = new Scanner(System.in);
-    System.out.println("Enter the year ");
-    int year = input.nextInt();
-    if(year % 4 == 0){
-      System.out.println("Enter the year ");
 
-  
+public class LeapYear {
+    public void printLeapYear(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the year : ");
+        int year = input.nextInt();
+        if (year % 4 == 0){
+            System.out.println("Entered year is a leap year : ");
+        }
+        else{
+            System.out.println("Entered year is not a leap year : ");
+           
+        }
+        input.close();
+    }
+    
+}
