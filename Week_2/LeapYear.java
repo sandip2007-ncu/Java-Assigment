9b498d83a134b899a44472645645246d5d12bc1f
sandip2007-ncu <sandip25csu215@ncuindia.edu>
2026-08-09 12:59:59 +0530

    Add LeapYear class to check for leap years

---
+++ Week_2/LeapYear.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class LeapYear{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the year ");
+    int year = input.nextInt();
+    if(year % 4 == 0){
+      System.out.println("Enter the year ");
+
+  
