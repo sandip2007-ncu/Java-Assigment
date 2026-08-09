c279e3a270b6c7164a2839aacd80cbd5c81f008b
sandip2007-ncu <sandip25csu215@ncuindia.edu>
2026-08-09 12:17:40 +0530

    Refactor GradingSystem to use user input for marks
    Refactored GradingSystem to accept user input for marks and removed the array of predefined marks. Updated the grading logic to directly print results based on user input.

--- Week_2/GradingSystem.java
+++ Week_2/GradingSystem.java
@@ -1,28 +1,26 @@
+import java.util.Scanner;
+
 public class GradingSystem{
-  public static void main(String[] args){
-    int[] marks = {-1,0,49,50,69,70,79,80,100,101};
-      for(int result : marks){
-          System.out.println(result + "  " + MarksChecking(result);
-      }
-  }
-  public static String MarksChecking(int marks){
-    if(marks >= 80 && marks <= 100 ){
-      return "Distinction";
-    }
-    else if(marks >= 70 && marks <= 79){
-      return "Merits";
-    }
-
-    else if(marks >= 50 && marks <= 69){
-      return "Pass";
-    }
-
-    else if(marks >= 0 && marks <= 49){
-      return "Fail";
-    } 
+  public static void main (String[] args){
     
-    else {
-      return "Invalid marks";
-    }
-  }
+  Scanner input = new Scanner(System.in);
+  System.out.println("Enter your marks : ");
+int marks = input.nextInt();
+    
+if(marks >= 80 && marks <= 100){
+  System.out.println("Distinction");
+}
+else if(marks >= 70 && marks < 80){
+  System.out.println("Merit");
+}
+else if(marks >= 50 && marks < 70){
+  System.out.println("Pass");
+}
+else if(marks >= 0 && marks < 50){
+  System.out.println("Fail");
+}
+else{
+  System.out.println("Invaild marks");
+}
+}
 }
