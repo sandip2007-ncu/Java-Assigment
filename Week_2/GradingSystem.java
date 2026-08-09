fd73493637435d699a7f17521ee492e5b61bb66f
sandip2007-ncu <sandip25csu215@ncuindia.edu>
2026-08-09 14:09:32 +0530

    Refactor GradingSystem to use printgrades method
    Refactored grading logic into a new method and updated grade thresholds.

--- Week_2/GradingSystem.java
+++ Week_2/GradingSystem.java
@@ -1,26 +1,19 @@
 import java.util.Scanner;
 
-public class GradingSystem{
-  public static void main (String[] args){
-    
-  Scanner input = new Scanner(System.in);
-  System.out.println("Enter your marks : ");
-int marks = input.nextInt();
-    
-if(marks >= 80 && marks <= 100){
-  System.out.println("Distinction");
-}
-else if(marks >= 70 && marks < 80){
-  System.out.println("Merit");
-}
-else if(marks >= 50 && marks < 70){
-  System.out.println("Pass");
-}
-else if(marks >= 0 && marks < 50){
-  System.out.println("Fail");
-}
-else{
-  System.out.println("Invaild marks");
-}
-}
+public class GradingSystem {
+    public void printgrades(){
+     Scanner sc=new Scanner(System.in);
+     System.out.println("Enter your marks");
+     int marks=sc.nextInt();
+     if(marks>90){
+        System.out.println("A");
+     } else if(marks>80){
+        System.out.println("B");
+     } else if (marks>70){
+        System.out.println("C");
+     } else  if(marks<33){ System.out.println("Pass");}
+     else { System.out.println("Fails");}
+ sc.close();
+    }
+   
 }
