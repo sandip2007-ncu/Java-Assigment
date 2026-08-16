c843e86e74ab864f527a675a48bf04e12b975ffb
sandip2007-ncu <sandip25csu215@ncuindia.edu>
2026-08-16 16:19:15 +0530

    Refactor grading logic and update input variable name

--- Week_2/GradingSystem.java
+++ Week_2/GradingSystem.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 
 public class GradingSystem {
     public void printgrades(){
-     Scanner sc=new Scanner(System.in);
+     Scanner input=new Scanner(System.in);
      System.out.println("Enter your marks");
-     int marks=sc.nextInt();
-     if(marks>90){
-        System.out.println("A");
-     } else if(marks>80){
-        System.out.println("B");
-     } else if (marks>70){
-        System.out.println("C");
-     } else  if(marks<33){ System.out.println("Pass");}
-     else { System.out.println("Fails");}
- sc.close();
+     int marks=input.nextInt();
+     if(marks>=80 && marks<=100){
+        System.out.println("Distinction");
+     } else if(marks>70 && marks<80){
+        System.out.println("Merit");
+     } else if (marks>=50 &&marks<70){
+        System.out.println("Pass");
+     } else  if(marks>=0 && marks<50){ System.out.println("Fail");}
+     else { System.out.println("Invalid Marks");}
+ input.close();
     }
    
 }
